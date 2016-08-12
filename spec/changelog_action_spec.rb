a5815f098f5d01d1c515aec920b06d1a989e9462
Pavel Procházka <pprochazka72@gmail.com>
2016-08-12 22:08:35 +0200

    Remove initial changelog spec

--- spec/changelog_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::ChangelogAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The changelog plugin is working!")
-
-      Fastlane::Actions::ChangelogAction.run(nil)
-    end
-  end
-end
