2de91b803f95e04ae106ae7245cc0b21145c305d
Pavel Procházka <pprochazka72@gmail.com>
2016-08-27 19:47:56 +0200

    Add unit tests for update_changelog action

---
+++ spec/update_changelog_action_spec.rb
@@ -0,0 +1,61 @@
+describe Fastlane::Actions::UpdateChangelogAction do
+  describe 'Update CHANGELOG.md action' do
+
+  	let (:changelog_mock_path) {'./fastlane-plugin-changelog/spec/fixtures/CHANGELOG_MOCK.md'}
+    let (:changelog_mock_path_hook) {'./spec/fixtures/CHANGELOG_MOCK.md'}
+    let (:updated_section_identifier) {'12.34.56'}
+    let (:existing_section_identifier) {'[0.0.5]'}
+
+    before(:each) do
+      @original_content = File.read(changelog_mock_path_hook)
+    end
+
+    after(:each) do 
+      File.open(changelog_mock_path_hook, 'w') {|f| f.write(@original_content) }
+    end 
+
+    it 'udpates [Unreleased] section identifier' do
+      # Read what's in [Unreleased]
+      read_result = Fastlane::FastFile.new.parse("lane :test do
+          read_changelog(changelog_path: '#{changelog_mock_path}')
+        end").runner.execute(:test)
+
+      # Update [Unreleased] with another section identifier
+		  update_result = Fastlane::FastFile.new.parse("lane :test do
+        	update_changelog(changelog_path: '#{changelog_mock_path}',
+                           updated_section_identifier: '#{updated_section_identifier}')
+      	end").runner.execute(:test)
+
+      # Read updated section
+      post_update_read_result = Fastlane::FastFile.new.parse("lane :test do
+          read_changelog(changelog_path: '#{changelog_mock_path}', 
+                         section_identifier: '[#{updated_section_identifier}]')
+        end").runner.execute(:test)
+        	
+      expect(read_result).to eq(post_update_read_result)
+    end
+
+    it 'udpates specific section identifier' do
+      # Read what's in [Unreleased]
+      read_result = Fastlane::FastFile.new.parse("lane :test do
+          read_changelog(changelog_path: '#{changelog_mock_path}',
+                         section_identifier: '#{existing_section_identifier}')
+        end").runner.execute(:test)
+
+      # Update given section identifier with another one
+      update_result = Fastlane::FastFile.new.parse("lane :test do
+          update_changelog(changelog_path: '#{changelog_mock_path}',
+                           section_identifier: '#{existing_section_identifier}',
+                           updated_section_identifier: '#{updated_section_identifier}')
+        end").runner.execute(:test)
+
+      # Read updated section
+      post_update_read_result = Fastlane::FastFile.new.parse("lane :test do
+          read_changelog(changelog_path: '#{changelog_mock_path}', 
+                         section_identifier: '[#{updated_section_identifier}]')
+        end").runner.execute(:test)
+          
+      expect(read_result).to eq(post_update_read_result)
+    end
+  end
+end
