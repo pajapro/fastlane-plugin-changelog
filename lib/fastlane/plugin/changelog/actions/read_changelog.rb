2df62eaff1d3725c9ef4a3f74c5ecd6b56ac103f
Pavel Procházka <pprochazka72@gmail.com>
2016-12-10 18:44:51 +0100

    Revert "Share path to changelog with other actions"
    This reverts commit 9bf6e4de3ee7173c0b0638d88df6acde02d4db40.

--- lib/fastlane/plugin/changelog/actions/read_changelog.rb
+++ lib/fastlane/plugin/changelog/actions/read_changelog.rb
@@ -2,7 +2,6 @@ module Fastlane
   module Actions
     module SharedValues
       READ_CHANGELOG_SECTION_CONTENT = :READ_CHANGELOG_SECTION_CONTENT
-      READ_CHANGELOG_CHANGELOG_PATH = :READ_CHANGELOG_CHANGELOG_PATH
     end
 
     class ReadChangelogAction < Action
@@ -42,7 +41,6 @@ module Fastlane
         UI.success("Finished reading #{section_identifier} section from '#{changelog_path}'") unless section_content.empty?
 
         Actions.lane_context[SharedValues::READ_CHANGELOG_SECTION_CONTENT] = section_content
-        Actions.lane_context[SharedValues::READ_CHANGELOG_CHANGELOG_PATH] = changelog_path
       end
 
       def self.remove_markdown(line, excluded_markdown_elements)
