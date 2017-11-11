b6cabd11a16bba44b3f4eeb38c43597751bea672
Pavel Procházka <pprochazka72@gmail.com>
2017-11-11 23:29:16 +0100

    Delete unused tests

--- spec/emojify_changelog_action_spec.rb
+++ spec/emojify_changelog_action_spec.rb
@@ -12,32 +12,5 @@ describe Fastlane::Actions::EmojifyChangelogAction do
 
       expect(result).to eq("Added 🎁\n- New awesome feature\n\nChanged ↔️\n- Onboarding flow\n\nFixed ✅\n- Fix Markdown links\n\nRemoved 🚫\n- User tracking\n\nWork In Progress 🚧\n- Sales screen\n\nSecurity 🔒\n- Enable SSL pinning\n\nDeprecated 💨\n- Obsolete contact screen\n\n")
     end
-
-    # it 'reads content of [Unreleased] section and includes header 3 (###)' do
-    #   result = Fastlane::FastFile.new.parse("lane :test do
-    #    	read_changelog(changelog_path: '#{changelog_mock_path}',
-    #    				   excluded_markdown_elements: '')
-    #  	end").runner.execute(:test)
-
-    #   expect(result).to eq("### Added\n- New awesome feature\n\n")
-    # end
-
-    # it 'reads content of [Unreleased] section and excludes list elements (-)' do
-    #   result = Fastlane::FastFile.new.parse("lane :test do
-    #    	read_changelog(changelog_path: '#{changelog_mock_path}',
-    #    				   excluded_markdown_elements: '-')
-    #  	end").runner.execute(:test)
-
-    #   expect(result).to eq("### Added\nNew awesome feature\n\n")
-    # end
-
-    # it 'reads content of [Unreleased] section and excludes list elements (-) and header 3 (###)' do
-    #   result = Fastlane::FastFile.new.parse("lane :test do
-    #    	read_changelog(changelog_path: '#{changelog_mock_path}',
-    #    				   excluded_markdown_elements: ['-', '###'])
-    #  	end").runner.execute(:test)
-
-    #   expect(result).to eq("Added\nNew awesome feature\n\n")
-    # end
   end
 end
