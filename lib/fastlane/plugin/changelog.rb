9ea76e3d8a37a430ddfeb9b85e924927a4c1808c
Pavel Procházka <pprochazka72@gmail.com>
2018-11-11 15:36:13 +0100

    Move methods from plugin class to helper class

--- lib/fastlane/plugin/changelog.rb
+++ lib/fastlane/plugin/changelog.rb
@@ -2,69 +2,12 @@ require 'fastlane/plugin/changelog/version'
 
 module Fastlane
   module Changelog
-    CHANGELOG_PATH = './CHANGELOG.md'
-    DEFAULT_CHANGELOG = '# Change Log
-All notable changes to this project will be documented in this file.
-
-The format is based on [Keep a Changelog](http://keepachangelog.com/)
-and this project adheres to [Semantic Versioning](http://semver.org/).
-
-## [Unreleased]
-### Added
-- Your awesome new feature'
 
     # Return all .rb files inside the "actions" and "helper" directory
     def self.all_classes
       Dir[File.expand_path('**/{actions,helper}/*.rb', File.dirname(__FILE__))]
     end
 
-    # Offer user to setup CHANGELOG.md file in project folder (unless it already exists)
-    def self.setup_changelog
-      unless has_changelog?
-        if FastlaneCore::UI.confirm('Your project folder does not have CHANGELOG.md - do you want to create one now?')
-          create_changelog
-
-          FastlaneCore::UI.message('Changelog plugin can automaticaly create a link for comparison between two tags (see https://github.com/pajapro/fastlane-plugin-changelog#--stamp_changelog)')
-          if FastlaneCore::UI.confirm('Do you want to create links for comparing tags?')
-            repo_url = FastlaneCore::UI.input('Enter your GitHub or Bitbucket repository URL (e.g.: https://github.com/owner/project or https://bitbucket.org/owner/project):')
-            create_comparison_link(repo_url)
-          else
-            write_to_changelog(DEFAULT_CHANGELOG)
-          end
-        end
-      end
-    end
-
-    # Does CHANGELOG.md exists in project folder?
-    def self.has_changelog?
-      File.exist?(CHANGELOG_PATH)
-    end
-
-    # Create CHANGELOG.md file
-    def self.create_changelog
-      FileUtils.touch 'CHANGELOG.md'
-    end
-
-    # Create a link for tag comparison
-    def self.create_comparison_link(repo_url)
-      if repo_url.start_with?('https://github.com')
-        output = DEFAULT_CHANGELOG + "\n\n[Unreleased]: #{repo_url}/compare/master...HEAD"
-        write_to_changelog(output)
-      elsif repo_url.start_with?('https://bitbucket.org')
-        output = DEFAULT_CHANGELOG + "\n\n[Unreleased]: #{repo_url}/compare/master..HEAD"
-        write_to_changelog(output)
-      else
-        FastlaneCore::UI.error('Unknown repository host')
-        FastlaneCore::UI.message('Creating CHANGELOG.md without links for comparing tags')
-        write_to_changelog(DEFAULT_CHANGELOG)
-      end
-    end
-
-    # Write given content to CHANGELOG.md
-    def self.write_to_changelog(changelog)
-      File.open(CHANGELOG_PATH, 'w') { |f| f.write(changelog) }
-      FastlaneCore::UI.success('Successfuly created CHANGELOG.md')
-    end
   end
 end
 
@@ -73,6 +16,3 @@ end
 Fastlane::Changelog.all_classes.each do |current|
   require current
 end
-
-# By default we want to ensure CHANGELOG.md is present
-Fastlane::Changelog.setup_changelog
--- lib/fastlane/plugin/changelog/helper/changelog_helper.rb
+++ lib/fastlane/plugin/changelog/helper/changelog_helper.rb
@@ -1,13 +1,59 @@
 module Fastlane
   module Helper
+    CHANGELOG_PATH = './CHANGELOG.md'
+    DEFAULT_CHANGELOG = '# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+### Added
+- Your awesome new feature'
+
     class ChangelogHelper
-      # class methods that you define here become available in your action
-      # as `Helper::ChangelogHelper.your_method`
-      #
-      def self.show_message
-        UI.message("Hello from the changelog plugin helper!")
+      # Generates CHANGELOG.md in project root
+      def self.generate_changelog
+        if FastlaneCore::UI.confirm('Your project folder does not have CHANGELOG.md - do you want to create one now?')
+            FileUtils.touch 'CHANGELOG.md'
+            generate_comparison_link
+        else 
+            FastlaneCore::UI.error("Cannot continue without CHANGELOG.md file")
+        end
       end
 
+      # Generates link for tag comparison
+      def self.generate_comparison_link     
+        FastlaneCore::UI.message('Changelog plugin can automaticaly create a link for comparison between two tags (see https://github.com/pajapro/fastlane-plugin-changelog#--stamp_changelog)')
+        if FastlaneCore::UI.confirm('Do you want to create links for comparing tags?')
+          repo_url = FastlaneCore::UI.input('Enter your GitHub or Bitbucket repository URL (e.g.: https://github.com/owner/project or https://bitbucket.org/owner/project):')
+          compose_comparison_link(repo_url)
+        else
+          write_to_changelog(DEFAULT_CHANGELOG)
+        end
+      end
+
+      # Composes link for tag comparison for GitHub or Bitbucket
+      def self.compose_comparison_link(repo_url)
+        if repo_url.start_with?('https://github.com')
+          output = DEFAULT_CHANGELOG + "\n\n[Unreleased]: #{repo_url}/compare/master...HEAD"
+          write_to_changelog(output)
+        elsif repo_url.start_with?('https://bitbucket.org')
+          output = DEFAULT_CHANGELOG + "\n\n[Unreleased]: #{repo_url}/compare/master..HEAD"
+          write_to_changelog(output)
+        else
+          FastlaneCore::UI.error('Unknown repository host')
+          FastlaneCore::UI.message('Creating CHANGELOG.md without links for comparing tags')
+          write_to_changelog(DEFAULT_CHANGELOG)
+        end
+      end
+
+      # Writes given content to CHANGELOG.md in project root
+      def self.write_to_changelog(changelog)
+        File.open(CHANGELOG_PATH, 'w') { |f| f.write(changelog) }
+        FastlaneCore::UI.success('Successfuly created CHANGELOG.md')
+      end
+      
       def self.get_line_separator(file_path)
         f = File.open(file_path)
         enum = f.each_char
